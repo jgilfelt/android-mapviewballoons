9a6dcfaebb40558aed52955ca08a1d2395af0773
Jeff Gilfelt <jgilfelt@gmail.com>
2011-11-21 19:29:34 +0000

    Updated project structure for custom example

---
+++ android-mapviewballoons-example/src/mapviewballoons/Main.java
@@ -0,0 +1,46 @@
+package mapviewballoons;
+
+import mapviewballoons.example.MyMap;
+import mapviewballoons.example.custom.CustomMap;
+import android.app.ListActivity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.AdapterView.OnItemClickListener;
+import android.widget.ArrayAdapter;
+
+public class Main extends ListActivity implements OnItemClickListener {
+
+	@Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        setListAdapter(new ArrayAdapter<String>(this,
+                android.R.layout.simple_list_item_1, mStrings));
+        getListView().setOnItemClickListener(this);
+    }
+
+    private String[] mStrings = {"Simple", "Custom"};
+
+	@Override
+	public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+		
+		Intent intent = null;
+		
+		switch (position) {
+		case 0:
+			intent = new Intent(this, MyMap.class);
+			startActivity(intent);
+			break;
+		case 1:
+			intent = new Intent(this, CustomMap.class);
+			startActivity(intent);
+			break;
+		default:
+			break;
+		}
+		
+	}
+	
+}
---
+++ android-mapviewballoons-example/src/mapviewballoons/example/custom/CustomMap.java
@@ -0,0 +1,95 @@
+/***
+ * Copyright (c) 2010 readyState Software Ltd
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may
+ * not use this file except in compliance with the License. You may obtain
+ * a copy of the License at
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ * 
+ */
+
+package mapviewballoons.example.custom;
+
+import java.util.List;
+
+import mapviewballoons.example.R;
+
+import android.graphics.drawable.Drawable;
+import android.os.Bundle;
+
+import com.google.android.maps.GeoPoint;
+import com.google.android.maps.MapActivity;
+import com.google.android.maps.MapController;
+import com.google.android.maps.MapView;
+import com.google.android.maps.Overlay;
+import com.google.android.maps.OverlayItem;
+
+public class CustomMap extends MapActivity {
+
+	MapView mapView;
+	List<Overlay> mapOverlays;
+	Drawable drawable;
+	Drawable drawable2;
+	CustomItemizedOverlay itemizedOverlay;
+	CustomItemizedOverlay itemizedOverlay2;
+	
+	@Override
+    public void onCreate(Bundle savedInstanceState) {
+		
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+        
+        mapView = (MapView) findViewById(R.id.mapview);
+		mapView.setBuiltInZoomControls(true);
+		
+		mapOverlays = mapView.getOverlays();
+		
+		// first overlay
+		drawable = getResources().getDrawable(R.drawable.marker);
+		itemizedOverlay = new CustomItemizedOverlay(drawable, mapView);
+		
+		GeoPoint point = new GeoPoint((int)(51.5174723*1E6),(int)(-0.0899537*1E6));
+		OverlayItem overlayItem = new OverlayItem(point, "Tomorrow Never Dies (1997)", 
+				"(M gives Bond his mission in Daimler car)");
+		itemizedOverlay.addOverlay(overlayItem);
+		
+		GeoPoint point2 = new GeoPoint((int)(51.515259*1E6),(int)(-0.086623*1E6));
+		OverlayItem overlayItem2 = new OverlayItem(point2, "GoldenEye (1995)", 
+				"(Interiors Russian defence ministry council chambers in St Petersburg)");		
+		itemizedOverlay.addOverlay(overlayItem2);
+		
+		mapOverlays.add(itemizedOverlay);
+		
+		// second overlay
+		drawable2 = getResources().getDrawable(R.drawable.marker2);
+		itemizedOverlay2 = new CustomItemizedOverlay(drawable2, mapView);
+		
+		GeoPoint point3 = new GeoPoint((int)(51.513329*1E6),(int)(-0.08896*1E6));
+		OverlayItem overlayItem3 = new OverlayItem(point3, "Sliding Doors (1998)", 
+				"(interiors)");
+		itemizedOverlay2.addOverlay(overlayItem3);
+		
+		GeoPoint point4 = new GeoPoint((int)(51.51738*1E6),(int)(-0.08186*1E6));
+		OverlayItem overlayItem4 = new OverlayItem(point4, "Mission: Impossible (1996)", 
+				"(Ethan & Jim cafe meeting)");		
+		itemizedOverlay2.addOverlay(overlayItem4);
+		
+		mapOverlays.add(itemizedOverlay2);
+		
+		final MapController mc = mapView.getController();
+		mc.animateTo(point2);
+		mc.setZoom(16);
+		
+    }
+	
+	@Override
+	protected boolean isRouteDisplayed() {
+		return false;
+	}
+
+}
